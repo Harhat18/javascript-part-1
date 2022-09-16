77eab0c0555715a72961ad0bc8fd3ed70a7e778e
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-09-16 22:33:33 +0300

    add

--- 9-uygulama_if_else/9-uygulama_if_else/script.js
+++ 9-uygulama_if_else/9-uygulama_if_else/script.js
@@ -1,64 +1,57 @@
 // 1- Bir sayının 10-50 arasında olup olmadığını kontrol ediniz.
-
-// let sayi1 = 49;
-// if (sayi1>10 && sayi1<50) {
-//     console.log("sayı 10-50 arasındadır.");
+// let sayi1 = 15;
+// if (sayi1 > 10 && sayi1 < 50) {
+//   console.log("sayı 10-50 arasındadır");
 // } else {
-//     console.log("sayı 10-50 arasında değildir.");
-// }
+// //   console.log("sayı 10-50 arasında değildir");
+// //
 
 // 2- Bir sayının pozitif tek sayı olup olmadığını kontrol ediniz.
-
 // let sayi = 21;
-// if (sayi % 2 == 1 || sayi > 0)
-// {
-//     console.log("sayi pozitif tek sayıdır.");
+// if (sayi % 2 == 1 || sayi > 0) {
+//   console.log("sayı pozitif tek sayıdır");
 // }
 
 // 3- x, y, z sayılarının büyüklük karşılaştırmasını yapınız. (else if 'i araştırınız.)
-
-// let x = 60, y = 60, z = 60;
-
+// let x = 60,
+//   y = 60,
+//   z = 60;
 // if (x > y && x > z) {
-//     console.log("x en büyük");
+//   console.log("x en büyük");
 // } else if (y > x && y > z) {
-//     console.log("y en büyük");
-// } else if (z > x && z > y) {
-//     console.log("y en büyük");
+//   console.log("y en büyük");
+// } else if (z > y && z > x) {
+//   console.log("z en büyük");
 // } else {
-//     console.log("sayılar eşit");
+//   console.log("sayılar eşit");
 // }
 
 // 4- 2 vize (40%) ve 1 final(60%) notuna göre hesaplanan ortalama için;
-
+// a-ortalama 50 ve üzeriyse geçti değilsa kaldı
 let vize1 = 10;
-let vize2 = 10;
+let vize2 = 20;
 let final = 70;
 
-let ortalama = (((vize1 + vize2) / 2) * 0.4) + (final * 0.6);
-
+let ortalama = ((vize1 + vize2) / 2) * 0.4 + final * 0.6;
 console.log("ortalamanız: " + ortalama);
-
-// a- Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazsın.
-
 // if (ortalama >= 50) {
-//     console.log("geçtiniz");
+//   console.log("geçtiniz");
 // } else {
-//     console.log("kaldınız");
+//   console.log("kaldınız");
 // }
 
-// b- Geçmek için ortalama 50 bile olsa final notu en az 50 olmalıdır.
+// b-geçmak için ortalama 50 olsa bile final notu en az 50 olmalı
 
-// if (ortalama >= 50 && final >= 50) {
-//     console.log("geçtiniz");
+// if (ortalama >= 50 && final >= 50 ) {
+//   console.log("geçtiniz");
 // } else {
-//     console.log("kaldınız");
+//   console.log("kaldınız");
 // }
 
-// c- Finalden 70 alındığında ortalama 50' nin altında olsa bile dersten geçilsin.
+// c-final 70 alındığında ortalama 50 olsada geçilsin
 
 if (ortalama >= 50 || final >= 70) {
-    console.log("geçtiniz");
+  console.log("geçtiniz");
 } else {
-    console.log("kaldınız");
+  console.log("kaldınız");
 }
