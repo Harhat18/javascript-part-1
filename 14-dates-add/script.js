32c3c2552f6f79e63b43e29f83f8236caabbe7aa
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-09-17 06:47:42 +0300

    add

--- 14-dates-add/script.js
+++ 14-dates-add/script.js
@@ -1,6 +1,6 @@
 let simdi = new Date(); // şimdiki tarih - saat
 
-// Get Methods
+// // Get Methods
 sonuc = simdi;
 sonuc = simdi.getDate();    // gün
 sonuc = simdi.getDay();     // 0: pazar 6: cumartesi
@@ -8,24 +8,24 @@ sonuc = simdi.getFullYear(); // yıl
 sonuc = simdi.getHours();   // saat
 sonuc = simdi.getTime();
 
-// Set Methods
+// // Set Methods
 // simdi.setFullYear(2025);
 simdi.setMonth(7);          // 0: Ocak
 simdi.setDate(15);
 
 sonuc = simdi;
 
-let dogumTarihi = new Date(1990, 5, 15);
+let dogumTarihi = new Date(1992, 8, 20);
 
 sonuc = simdi.getFullYear() - dogumTarihi.getFullYear();
 
-let milisecond = simdi - dogumTarihi;
-let saniye = milisecond / 1000;     // saniye
-let dakika = saniye / 60;
-let saat = dakika / 60;
-let gun = saat / 24;
+// let milisecond = simdi - dogumTarihi;
+// let saniye = milisecond / 1000;     // saniye
+// let dakika = saniye / 60;
+// let saat = dakika / 60;
+// let gun = saat / 24;
 
-sonuc = gun;
+// sonuc = gun;
 
 console.log(sonuc);
 console.log(typeof sonuc);
