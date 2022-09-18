f03ebe9e1256ecd9471ede77de381309f1d915be
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-09-18 18:28:20 +0300

    add

--- 16-array_methods/script.js
+++ 16-array_methods/script.js
@@ -2,25 +2,25 @@ let ogrenciler = ["çınar","yiğit","ada"];
 
 sonuc = ogrenciler.length;
 
-// array to string
+// // array to string
 sonuc = ogrenciler.toString();
-sonuc = ogrenciler.join(" ");
+// sonuc = ogrenciler.join(" ");
 
-// eleman silme
-// sonuc = ogrenciler.pop();       // son eleman silinir ve silinen eleman geri döndürülür.
-// sonuc = ogrenciler.shift();     // ilk eleman silinir.
+// // eleman silme
+// // sonuc = ogrenciler.pop();       // son eleman silinir ve silinen eleman geri döndürülür.
+// // sonuc = ogrenciler.shift();     // ilk eleman silinir.
 
-// eleman ekleme
-// sonuc = ogrenciler.push("sena");    // dizinin sonuna eleman eklenir.
-// sonuc = ogrenciler.unshift("sena");
+// // eleman ekleme
+// // sonuc = ogrenciler.push("sena");    // dizinin sonuna eleman eklenir.
+// // sonuc = ogrenciler.unshift("sena");
 
-let markalar1 = ["mazda","toyota"];
-let markalar2 = ["opel","renault"];
-let markalar3 = ["mercedes"];
+// let markalar1 = ["mazda","toyota"];
+// let markalar2 = ["opel","renault"];
+// let markalar3 = ["mercedes"];
 
-// sonuc = markalar1.concat(markalar2, markalar3);
-// sonuc = markalar1.splice(0, 1, "bmw","audi");
-sonuc = markalar1.splice(0, 1);
+// // sonuc = markalar1.concat(markalar2, markalar3);
+// // sonuc = markalar1.splice(0, 1, "bmw","audi");
+// sonuc = markalar1.splice(0, 1);
 
-console.log(sonuc);
-console.log(markalar1);
+// console.log(sonuc);
+// console.log(markalar1);
